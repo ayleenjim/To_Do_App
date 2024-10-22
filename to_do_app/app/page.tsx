18697ad72c1065caf5b634c756648543aaafd078
Ayleen Jimenez <ayleen.jimenez01@outlook.com>
2024-10-21 22:54:14 -0500

    CREATE static Todo list and ADD to home page

--- to_do_app/app/page.tsx
+++ to_do_app/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 // testing bootstrap
 import { Button } from 'react-bootstrap';
+import TodoList from "./components/toDoList";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Hello from Ayleen</h1>
+        <TodoList />
         <Button variant="primary">Click me (I do nothing. -Ayleen)!</Button>
         <Image
           className={styles.logo}
