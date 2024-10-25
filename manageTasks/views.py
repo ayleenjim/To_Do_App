be1ed57638d9c9c6479c56a3d36ccdf9294d7787
Ayleen Jimenez <ayleen.jimenez01@outlook.com>
2024-10-24 19:57:53 -0500

    ADD root URL handling and 'task' API

--- manageTasks/views.py
+++ manageTasks/views.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the ManageTasks API. Navigate to /api/ for the API endpoints.")
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
